ba29febde9c90c57e801bee309a98e3772bad074
Danny Nhoisaykham <dannynhois@gmail.com>
2022-01-10 19:29:20 -0600

    add more test for command line parser

--- mvdictionary.test/CommandLineParserTests.cs
+++ mvdictionary.test/CommandLineParserTests.cs
@@ -55,4 +55,55 @@ public class CommandLineParserTests
 
         _dictionary.Verify(x => x.GetMembers(It.IsAny<string>()),Times.AtLeastOnce);
     }
+    
+    [Test]
+    public void CommandLineParser_KeysCallsCorrectFunction()
+    {
+        // Given
+        var parser = new CommandLineParser(_dictionary.Object);
+        parser.SetValues("keys ignore").Parse();
+        _dictionary.Verify(x => x.GetKeys(),Times.AtLeastOnce);
+    }
+    [Test]
+    public void CommandLineParser_MembersCallsCorrectFunction()
+    {
+        // Given
+        var parser = new CommandLineParser(_dictionary.Object);
+        parser.SetValues("members key").Parse();
+        _dictionary.Verify(x => x.GetMembers("key"),Times.AtLeastOnce);
+    }
+    [Test]
+    public void CommandLineParser_AddCallsCorrectFunction()
+    {
+        // Given
+        var parser = new CommandLineParser(_dictionary.Object);
+        parser.SetValues("add foo bar").Parse();
+        _dictionary.Verify(x => x.AddItem("foo","bar"),Times.AtLeastOnce);
+    }
+    [Test]
+    public void CommandLineParser_RemoveCallsCorrectFunction()
+    {
+        // Given
+        var parser = new CommandLineParser(_dictionary.Object);
+        parser.SetValues("remove foo bar").Parse();
+        _dictionary.Verify(x => x.RemoveItem("foo","bar"),Times.AtLeastOnce);
+    }
+    
+    [Test]
+    public void CommandLineParser_RemoveAllCallsCorrectFunction()
+    {
+        // Given
+        var parser = new CommandLineParser(_dictionary.Object);
+        parser.SetValues("REMOVEALL foo").Parse();
+        _dictionary.Verify(x => x.RemoveAllItem("foo"),Times.AtLeastOnce);
+    }
+    [Test]
+    public void CommandLineParser_ClearCallsCorrectFunction()
+    {
+        // Given
+        var parser = new CommandLineParser(_dictionary.Object);
+        parser.SetValues("CLEAR foo").Parse();
+        _dictionary.Verify(x => x.Clear(),Times.AtLeastOnce);
+    }
+    
 }
